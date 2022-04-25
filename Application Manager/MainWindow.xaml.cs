8bbeaac6c73fc5684d3a6f6e30117869aea54c5c
Timalice1 <newacccc2@gmail.com>
2022-04-25 17:45:43 +0300

    Checked mashine for installed apps

--- Application Manager/MainWindow.xaml.cs
+++ Application Manager/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +22,44 @@ namespace Application_Manager {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+        }
+
+        List<string> pathes = new List<string>();
+        string uninstallFile = "";
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            string uninstallKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+            using (RegistryKey rk = Registry.LocalMachine.OpenSubKey(uninstallKey)) {
+                foreach (string skName in rk.GetSubKeyNames()) {
+                    using (RegistryKey sk = rk.OpenSubKey(skName)) {
+                        try {
+                            var name = sk.GetValue("DisplayName") as string;
+                            var path = sk.GetValue("InstallLocation") as string;
+                            if (string.IsNullOrEmpty(path) || !Directory.Exists(path)) {
+                                continue;
+                            }
+
+                            pathes.Add(path);
+                            appList.Items.Add(name);
+                        }
+                        catch (Exception ex) { }
+                    }
+                }
+            }
+        }
+
+        private void appList_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            try {
+                var index = appList.SelectedIndex;
+                var files = Directory.GetFiles(pathes[index]);
+                if (files.Contains($@"{pathes[index]}unins000.exe")) {
+                    btnUninstall.IsEnabled = true;
+                    uninstallFile = $@"{pathes[index]}unins000.exe";
+                }
+                else btnUninstall.IsEnabled = false;
+            }catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
